2a2aa8436bf850ed33580236a05cd1a3226e6019
Muhammed Sinan P <iam.muhammedsinan.p@gmail.com>
2025-08-09 21:48:11 +0530

    force dynamic rendering for certificationsSection and projectsSection

--- src/app/(main)/@certificationsSection/page.tsx
+++ src/app/(main)/@certificationsSection/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { fetchCertifications } from "@/services/certificationService";
 import CertificationsContainer from "./CertificationsContainer";
 
--- src/app/(main)/@projectsSection/page.tsx
+++ src/app/(main)/@projectsSection/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import { fetchAllProjects } from "@/services/projectService";
 import ProjectsContainer from "./ProjectsContainer";
 
